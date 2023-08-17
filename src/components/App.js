cf3cda99764ac84d97b51551f07d2e5741939624
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-08-17 13:38:50 -0700

    complete basic style for display and wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
+        <h1>React Calculator</h1>
+      <div className="calculator-wrapper">
+        {/* Display component */}
+        <Display />
+        {/* Buttons component */}
+      </div>
 
     </div>
   );
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Display() {
+  return (
+    <div className="display"><p className="calculation-display">1232</p></div>
+  )
+}
+
+export default Display
