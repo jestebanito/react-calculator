0e4e873c097577bf0a688f0a6c440110d12b3b19
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-08-17 14:09:29 -0700

    fixed the scroll bar

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Display() {
   return (
-    <div className="display"><p className="calculation-display">1232</p></div>
+    <div className="box">
+      <div className="display"> 
+      <p className="calculation-display">1232102312313212313213213213213a2s1d32as1d32as1d32a1s32d1as23d1a23s1d32as123d1as23d23as1</p>
+      </div>
+    </div>
   )
 }
 
