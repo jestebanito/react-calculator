f5eeebbafabcb5183ff41ff8db229389b14e6f90
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-08-28 12:50:44 -0700

    fixed bugs around the operators and AC, decimal, preventing multiple operation

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import ButtonPad from "./ButtonPad";
 
 // let decimalExisted = false;
+let operatorExisted = false;
 
 function App() {
   // use this state to update the value on display
@@ -23,7 +24,7 @@ function App() {
         processNumberPress(buttonData.value);
         break;
       default:
-        case "decimal":
+      case "decimal":
         processDecimalPress(buttonData.value);
         break;
       case "sign":
@@ -41,16 +42,18 @@ function App() {
     }
 
     // function processNumberPress(number) {
-      // if (displayString === "0") {
-      //   setDisplayString(`${number}`);
-      // } else {
-      //   setDisplayString(`${displayString}${number}`);
-      // }
-    // }    
+    // if (displayString === "0") {
+    //   setDisplayString(`${number}`);
+    // } else {
+    //   setDisplayString(`${displayString}${number}`);
+    // }
+    // }
 
     function processNumberPress(number) {
       // Added an if statement for when an operation is completed
-      if (operationCompleted) {
+      if (operationCompleted && operatorExisted) {
+        setDisplayString(`${displayString} ${number} `);
+      } else if (operationCompleted) {
         setDisplayString(`${number}`);
         setOperationCompleted(false); // Reset operationCompleted
       } else if (displayString === "0") {
@@ -59,7 +62,7 @@ function App() {
         setDisplayString(`${displayString}${number}`);
       }
     }
-    
+
     // function processDecimalPress(decimal) {
     //   if (decimalExisted === false) {
     //     console.log(decimalExisted);
@@ -74,64 +77,67 @@ function App() {
     function processDecimalPress(decimal) {
       // Check if the last character in the displayString is an operator
       const lastCharIsOperator = "+-*/%√".includes(lastCharacter);
-    
+
       // Check if a decimal point already exists in the current number
       const decimalPointAlreadyExists = displayString.includes(".");
-    if (!decimalPointAlreadyExists || lastCharIsOperator) {
+      if (!decimalPointAlreadyExists || lastCharIsOperator) {
         // If there's no decimal point in the current number or the last character is an operator,
         // you can add the decimal point.
         setDisplayString(`${displayString}${decimal}`);
       }
     }
-    
+
     function processOperatorPress(operator) {
-      if (operationCompleted) {
+      if (!operatorExisted) {
         setDisplayString(`${displayString} ${operator} `);
-        setOperationCompleted(false);
+        // setOperationCompleted(false);
         if (operator !== "%" && operator !== "\u221a") {
           setOperatorDisplay(operator);
         }
+        operatorExisted = true;
       } else {
         // Add the operator to the displayString
-        if (buttonData.type === "operator") {
-          setDisplayString(`${displayString}${operator}`);
-          if (operator !== "%" && operator !== "\u221a") {
-            setOperatorDisplay(operator);
-          }
-          if (operator === "\u221a") {
-            setDisplayString(displayString.slice(0, -1) + operator);
-          }
-        }
+        // if (operator !== "%" && operator !== "\u221a") {
+        //   setOperatorDisplay(operator);
+        // }
+        // if (operator === "\u221a") {
+        //   setDisplayString(displayString.slice(0, -1) + operator);
+        // }
       }
     }
-    
 
     function processEnterPress(enter) {
       if (enter === "=") {
+        operatorExisted = false;
         // Replace Unicode division symbol with actual '/'
         let evaluatedString = displayString
-        .replace(/\u00f7/g, '/')
-        .replace(/\u00d7/g, '*')
+          .replace(/\u00f7/g, "/")
+          .replace(/\u00d7/g, "*");
         // Check if square root operator is present
-        if (evaluatedString.includes('\u221a')) {
-          evaluatedString = evaluatedString.replace(/\u221a/g, 'Math.sqrt(') + ')';
+        if (evaluatedString.includes("\u221a")) {
+          evaluatedString =
+            evaluatedString.replace(/\u221a/g, "Math.sqrt(") + ")";
         }
         // Check if percent symbol is present
-        if (evaluatedString.includes('%')) {
-          // Create variable to set evaluatedString to the formula on line 91 OUTSIDE of the arrow function scope
-          let tempEvaluatedString = evaluatedString;
-          // Check if percent symbol is present after a number
-          const percentRegex = /(\d+)\s*%/g;
-          evaluatedString = evaluatedString.replace(percentRegex, (_, num, index, input) => {
-            const percentage = parseInt(num, 10) * 0.01;
-            const precedingNumber = parseFloat(input.substring(0, index));
-            tempEvaluatedString = `(${precedingNumber} ${operatorDisplay} (${precedingNumber} * ${percentage}))`;
-            return '';
-          });
-          evaluatedString = tempEvaluatedString;
+        if (evaluatedString.includes("%")) {
+          // // Create variable to set evaluatedString to the formula on line 91 OUTSIDE of the arrow function scope
+          // let tempEvaluatedString = evaluatedString;
+          // // Check if percent symbol is present after a number
+          // const percentRegex = /(\d+)\s*%/g;
+          // evaluatedString = evaluatedString.replace(
+          //   percentRegex,
+          //   (_, num, index, input) => {
+          //     const percentage = parseInt(num, 10) * 0.01;
+          //     const precedingNumber = parseFloat(input.substring(0, index));
+          //     tempEvaluatedString = `(${precedingNumber} ${operatorDisplay} (${precedingNumber} * ${percentage}))`;
+          //     return "";
+          //   }
+          // );
+          // evaluatedString = tempEvaluatedString;
+        
         }
         try {
-          console.log(evaluatedString)
+          console.log(evaluatedString);
           let result = eval(evaluatedString);
           setDisplayString(result);
         } catch (error) {
@@ -161,6 +167,11 @@ function App() {
     function processClearPress(clear) {
       if (clear === "AC") {
         setDisplayString("0");
+        operatorExisted = false;
+        setOperationCompleted(false);
+        setSignDisplay("");
+        setSignValue(1);
+        setOperatorDisplay("");
       } else if (clear === "C") {
         if (displayString.length === 2 && signDisplay === "-") {
           setDisplayString("0");
